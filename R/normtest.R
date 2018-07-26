f753639b0d10ad5e8c090178e74ae486078d4264
karthickrajas <karthick11b36@gmail.com>
2018-07-27 01:14:42 +0530

    added Normtest function

---
+++ R/normtest.R
@@ -0,0 +1,20 @@
+#' Normtest
+#'
+#'Performing Test for normality
+#' @param a list of numbers to be tested for normality
+#'
+#' @return Returns the dataframe having shapiro test, ad test, cvm test, ll test, sf test
+#'
+#'
+#' @export
+
+normtest<-function(x){
+  library(nortest)
+  s<-shapiro.test(x)
+  ad<-ad.test(x)
+  cvm<-cvm.test(x)
+  ll<-lillie.test(x)
+  sf<-sf.test(x)
+  df<-data.frame(Method=c(s$method, ad$method, cvm$method, ll$method, sf$method),P.Value=c(s$p.value, ad$p.value, cvm$p.value, ll$p.value, sf$p.value))
+  return(df)
+}
