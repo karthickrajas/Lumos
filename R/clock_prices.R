5b4612e7e6991bfeefafef4f25d26ccd38d4e961
karthickrajas <karthick11b36@gmail.com>
2018-07-27 20:54:39 +0530

    Adding clock prices rda file

--- R/clock_prices.R
+++ R/clock_prices.R
@@ -1,6 +1,6 @@
-#'Clock Prices data
+#'@title Clock Prices data
 #'
-#'Clock price have 1 dependent variable price and 2 independent variable price and number of bidders.
+#'@description Clock price have 1 dependent variable price and 2 independent variable price and number of bidders.
 #'
 #'@docType data
 #'
